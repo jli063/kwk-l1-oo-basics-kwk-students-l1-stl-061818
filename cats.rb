9b8787646a9a93f31c3c3fc46d1a7b6fe0ac3a31
jli063 <jli063@rsdmo.org>
2018-06-22 15:31:25 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -31,5 +31,5 @@ tiger = Cats.new("very", "pretty fuzzy", "super duper mean")
 garfield = Cats.new("has a bit of an attitude","up to debate","yes, he's hangry all the time")
 # puts "Is Garfield cute? #{garfield.is_it_cute}"
 
-puts "Is Garfield mean? #{garfield.is_it_mean}"
-puts "Is Garfield fuzzy? #{garfield.is_it_fuzzy}"
+puts "Is tiger mean? #{tiger.is_it_mean}"
+puts "give me some basic facts about tiger. #{tiger.basic_facts}"
