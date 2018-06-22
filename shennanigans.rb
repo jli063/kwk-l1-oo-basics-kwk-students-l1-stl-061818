80d48bec3a74c6cb4133540763ff5435fffb667b
jli063 <jli063@rsdmo.org>
2018-06-22 15:57:30 +0000

    automatically backed up by learn

--- shennanigans.rb
+++ shennanigans.rb
@@ -1,13 +1,18 @@
 class shenanigans
-  def initialize(girl, necklace, earrings, teens)
-    @girl = girl
+  def initialize(hair, necklace, earrings, )
+    @hair = hair
     @necklace = necklace
     @earrings = earrings
-    @teens = teens
+    @
   end
   
   def is_she_a_girl
     @girl
   end
-  
+  def does_she_wear_a_necklace
+     
+  lily = shenanigans.new("red hair", "layers","double gold shapes","")
+  jessica = shenanigans.new("straight black hair", "marble","double gold balls",)
+  alice = shenanigans.new("curly brown hair", "pendant","dangly",)
+  sam = shenanigans.new("wavy black hair", "circle", "pearls",)
 end
