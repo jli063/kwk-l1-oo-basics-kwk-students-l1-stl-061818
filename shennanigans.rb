f6ba178100bf12ed4592f3467098afeecdfa72d8
jli063 <jli063@rsdmo.org>
2018-06-22 15:51:04 +0000

    automatically backed up by learn

--- shennanigans.rb
+++ shennanigans.rb
@@ -1,2 +1,9 @@
 class shenanigans
-  def initialize(girl, necklace, earrings, )
+  def initialize(girl, necklace, earrings, teens)
+    @girl = girl
+    @necklace = necklace
+    @earrings = earrings
+    @teens = teens
+  end
+  
+end
