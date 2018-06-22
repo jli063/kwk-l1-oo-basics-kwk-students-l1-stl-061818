221c5f3dc9f6ecf1422eace0247f66f1a1da9f3a
jli063 <jli063@rsdmo.org>
2018-06-22 16:42:40 +0000

    automatically backed up by learn

--- shennanigans.rb
+++ shennanigans.rb
@@ -16,6 +16,10 @@ class Shenanigans
   def what_are_her_earrings
     @earrings
   end
+  
+  def basic_facts
+    puts "She is a girl in high school who is wearing jewelry"
+  end
     
 end
 # girls, high school, jewelry
@@ -25,7 +29,7 @@ jessica = Shenanigans.new("straight black", "marble","double gold balls")
 alice = Shenanigans.new("curly brown", "pendant","dangly")
 sam = Shenanigans.new("wavy black", "circle", "pearls")
   
-puts "What color is Lily's hair? #{lily.what_is_her_hair_like}. What are her necklaces like? #{lily.what_is_her_necklace}.What are her earrings? #{lily.what_are_her_earrings}."
-puts "What color is Jessica's hair? #{jessica.what_is_her_hair_like}. What are her necklaces like? #{jessica.what_is_her_necklace}. What are her earrings? #{jessica.what_are_her_earrings}"
-puts "What color is Alice's hair? #{alice.what_is_her_hair_like}. What are her necklaces like? #{alice.what_is_her_necklace}. What are her earrings? #{alice.what_are_her_earrings}"
-puts "What color is Sam's hair? #{sam.what_is_her_hair_like}. What are her necklaces like? #{sam.what_is_her_necklace}. What are her earrings? #{sam.what_are_her_earrings}"
+puts "What color is Lily's hair? #{lily.what_is_her_hair_like}. What are her necklaces like? #{lily.what_is_her_necklace}.What are her earrings? #{lily.what_are_her_earrings}.#{basic_facts)"
+puts "What color is her hair? #{jessica.what_is_her_hair_like}. What are her necklaces like? #{jessica.what_is_her_necklace}. What are her earrings? #{jessica.what_are_her_earrings}. #{basic_facts}"
+puts "What color is Alice's hair? #{alice.what_is_her_hair_like}. What are her necklaces like? #{alice.what_is_her_necklace}. What are her earrings? #{alice.what_are_her_earrings} #{basic_facts}"
+puts "What color is Sam's hair? #{sam.what_is_her_hair_like}. What are her necklaces like? #{sam.what_is_her_necklace}. What are her earrings? #{sam.what_are_her_earrings} #{basic_facts}"
